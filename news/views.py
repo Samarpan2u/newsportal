e8ac499ef97ddf622047a0e052b7cad14c9d8ab4
sam <samarpan2u@gmail.com>
2024-07-14 22:59:13 +0545

    created template and vies for homepage

--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    context = {
+        "welcome" : "welcome to my custom page: dhoom tanananananaa" 
+    }
+    templates= "index.html"
+    return render(request, templates, context)  

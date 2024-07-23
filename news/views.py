e199ea21b01749b5cfdcd2d04e8013a1b8947910
sam <samarpan2u@gmail.com>
2024-07-23 22:37:06 +0545

    FEAT : newsportal trending news and editors pick

--- news/views.py
+++ news/views.py
@@ -4,9 +4,13 @@ from news.models import News
 # Create your views here.
 def home_page(request):
     editor_picks = News.objects.filter(is_editorial=True)
+    trending_news = News.objects.all().order_by('-views_count')[:4]
+    editor_picks_new = News.objects.filter(is_editorial=True)[:4]
     context = {
         "welcome" : "welcome to my custom page: dhoom tanananananaa",
-        "editorial" : editor_picks
+        "editorial" : editor_picks,
+        "trending_news" : trending_news, 
+        "editor_picks_new" : editor_picks_new
                         }
     templates= "base.html"
     return render(request, templates, context)  

a36e8368a3436a29f07645102f3a8836adbf5693
sam <samarpan2u@gmail.com>
2024-07-25 21:48:55 +0545

    FEAT : newsportal popular and recent news

--- news/views.py
+++ news/views.py
@@ -6,11 +6,15 @@ def home_page(request):
     editor_picks = News.objects.filter(is_editorial=True)
     trending_news = News.objects.all().order_by('-views_count')[:4]
     editor_picks_new = News.objects.filter(is_editorial=True)[:4]
+    popular_news= News.objects.all().order_by('views_count')[:5]
+    recent_news= News.objects.all().order_by('created_date')[:5]
     context = {
         "welcome" : "welcome to my custom page: dhoom tanananananaa",
         "editorial" : editor_picks,
         "trending_news" : trending_news, 
-        "editor_picks_new" : editor_picks_new
+        "editor_picks_new" : editor_picks_new,
+        "popular_news" : popular_news,
+        "recent_news" :recent_news
                         }
     templates= "base.html"
     return render(request, templates, context)  

acb9c5e19f386cbf8117098f85caf1c8736a078e
sam <samarpan2u@gmail.com>
2024-08-06 22:17:56 +0545

    FEAT : newsportal category

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from news.models import News
+from news.models import News, Category
 from datetime import datetime
 from news.forms import ContactUsForm
 
@@ -11,6 +11,10 @@ def home_page(request):
     editor_picks_new = News.objects.filter(is_editorial=True)[:4]
     popular_news= News.objects.all().order_by('views_count')[:5]
     recent_news= News.objects.all().order_by('created_date')[:5]
+    catgories= Category.objects.all()
+    category_news = {
+        category.name : News.objects.filter(category=category) for category  in catgories # catergory=i , (category.name= for category)
+    }
     context = {
         "welcome" : "welcome to my custom page: dhoom tanananananaa",
         "date_time" : current_datetime,
@@ -18,7 +22,8 @@ def home_page(request):
         "trending_news" : trending_news, 
         "editor_picks_new" : editor_picks_new,
         "popular_news" : popular_news,
-        "recent_news" :recent_news
+        "recent_news" :recent_news,
+        "category_news" : category_news
                         }
     templates= "base.html"
     return render(request, templates, context)  
